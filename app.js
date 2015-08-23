da97be699eea83aeeacf5429f827a6465d265f17
Andrew Evans <andrew.j.c.e@gmail.com>
2015-08-23 14:10:09 -0400

    Added facebook authentication & account creation

--- app.js
+++ app.js
@@ -3,10 +3,66 @@
 var express = require('express');
 var mongoose = require('mongoose');
 var passport = require('passport');
-var facebookStrategy = require('passport-facebook').Strategy;
+var findOrCreate = require('mongoose-findorcreate');
+var FacebookTokenStrategy = require('passport-facebook-token').Strategy;
 var qs = require('querystring');
 
 var app = express();
+
+//provider: 'facebook',
+//  id: '10153494206687533',
+//  displayName: 'Andy Evans',
+//  name: { familyName: '', givenName: '', middleName: '' },
+//  gender: '',
+//  emails: [ { value: '' } ],
+//  photos: [ { value: 'https://graph.facebook.com/10153494206687533/picture?type=large' } ],
+//  _raw: '{"name":"Andy Evans","id":"10153494206687533"}',
+//  _json: { name: 'Andy Evans', id: '10153494206687533' } }
+
+
+var FACEBOOK_APP_ID = "1678602979038851";
+var FACEBOOK_APP_SECRET = "a4a9e8bf1e5b2e90928aeffec02cf3a5";
+ 
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(user, done) {
+  done(null, user);
+});
+
+//Setup Facebook Authentication by Token
+passport.use(new FacebookTokenStrategy({
+    clientID: "1678602979038851",
+    clientSecret: "a4a9e8bf1e5b2e90928aeffec02cf3a5",
+  },
+  function(accessToken, refreshToken, profile, done) {
+	console.log(profile);
+    User.findOrCreate({ 
+		provider  :  profile.provider,
+  		id   :  profile.id,
+		displayName  :  profile.displayName,
+		familyName  :  profile.familyName,
+		givenName  :  profile.givenName
+	}, function (err, user) {
+		return done(err, user);
+    });
+  }
+));
+
+var UserSchema = new mongoose.Schema({
+    provider  :  String,
+  	id   :  String,
+	displayName  :  String,
+	familyName  :  String,
+	givenName  :  String
+});
+
+UserSchema.plugin(findOrCreate);
+var User = mongoose.model('User', UserSchema);
 
 //define routes - this needs to be broken out into a routes directory
 app.get('/', function(req, res){
@@ -15,19 +71,44 @@ app.get('/', function(req, res){
 
 
 //define routes - this needs to be broken out into a routes directory
-app.post('/login/facebook', function(req, res){
-    var body = '';
-    req.on('data', function (data) {
-        body += data;
-        // Too much POST data, kill the connection!
-        if (body.length > 1e6)
-            req.connection.destroy();
-    });
-    req.on('end', function () {
-        var post = qs.parse(body);
-        console.log(post);
-        // use post['blah'], etc.
-    });
+//app.post('/login/facebook', function(req, res){	
+//    var body = '';
+//    req.on('data', function (data) {
+//        body += data;
+//        // Too much POST data, kill the connection!
+//        if (body.length > 1e6)
+//            req.connection.destroy();
+//    });
+//    req.on('end', function () {
+//        var post = JSON.parse(body);
+//		var access_token = post.access_token;
+//		console.log(access_token);
+//		var req_2 = body;
+//		
+//		passport.authenticate('facebook-token'), function (req_2, res) {
+//			console.log("hm");
+//			res.send(req_2.user? 200 : 401);
+//		}
+//		console.log("gr");
+//    });
+//});
+
+app.post('/login/facebook', 
+		 passport.authenticate('facebook-token'), 
+		 function (req, res) {
+			console.log(req);
+			console.log(res);
+			if (req.user){
+				//you're authenticated! return sensitive secret information here.
+				console.log("you're authenticated!");
+				console.log(req.user);
+				console.log(req.session);
+				res.sendStatus(200);
+			} else {
+				// not authenticated. go away.
+				console.log("not authenticated. go away.");
+				res.sendStatus(401);
+			}
 });
 
 app.get('/hello', function(req, res){
